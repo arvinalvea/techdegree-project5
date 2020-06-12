947c2d17bc9368aadac6df273675b1ecbad6ad05
Arvin Alvea <arvinalvea@gmail.com>
2020-06-12 17:14:07 -0400

    Added "no photo found" feature

--- js/scripts.js
+++ js/scripts.js
@@ -4,16 +4,23 @@ let userInput;
 
 document.getElementById("pic-search").addEventListener("keyup", function() {
   
+
   let input = document.getElementById("pic-search");  // Receives user input from search form
     userInput = input.value.toLowerCase() 
     
     $('a').each(function(index, element) {            // Loops through 'a' elements
 
+      let hidden = $('.item[style="display: none;"]');
       let caption = document.getElementsByTagName("a")[index].getAttribute("data-title");    // Targets caption from 'a' element
       let match = caption.includes(userInput);
       
       if (match != true || match === null) {
         $('.item').eq(index).hide();
+        if ($('.item').length === hidden.length) {
+          $('#noPhoto').removeAttr('hidden');;
+           } else {
+             $('#noPhoto').attr('hidden', true);
+           }
       } else {
         $('.item').eq(index).show();
       } 
@@ -22,7 +29,15 @@ document.getElementById("pic-search").addEventListener("keyup", function() {
     
 });
             
-
+let hidden = $('.item[style="display: none;"]');
+if (match != true || match === null) {
+  $('.item').eq(index).hide();
+    if ($('.item').length === hidden.length) {
+    // Code here to add "No Photos Found" message.
+     }
+} else {
+  $('.item').eq(index).show();
+} 
 
 
 
