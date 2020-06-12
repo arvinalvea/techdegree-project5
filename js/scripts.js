0b235a311c7d88429bfef7f548554bc4697aac37
Arvin Alvea <arvinalvea@gmail.com>
2020-06-12 09:33:53 -0400

    Updated thumbnail imgs + mobile layout

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,38 @@
 let userInput;
+let notFound = [];
+
 
 document.getElementById("pic-search").addEventListener("keyup", function() {
+  
   let input = document.getElementById("pic-search");
     userInput = input.value.toLowerCase()
-    console.log(userInput);
     
     $('a').each(function(index, element) {
 
       let caption = document.getElementsByTagName("a")[index].getAttribute("data-title");
       let match = caption.includes(userInput);
-      console.log(match);
-  
+      
+
       if (match != true || match === null) {
         $('.item').eq(index).hide();
+        notFound.push('not found');
       } else {
         $('.item').eq(index).show();
-      }
+        notFound.pop();
+      } 
+
+      
+      console.log(notFound.length);
+
+      // if (notFound.length > 12) {
+      //   $('#noPhotos').show(); 
+      // } else {
+      //   $('#noPhotos').hide();
+      // }
+
+    });
     
-    });  
+    
   
 });
             
