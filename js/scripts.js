c7fda3097934e915614dc45899d2e25cf631b7e3
Arvin Alvea <arvinalvea@gmail.com>
2020-06-12 10:23:52 -0400

    Added comments in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,25 @@
-let userInput;
-let notFound = [];
+// Search content functionality
 
+let userInput;
 
 document.getElementById("pic-search").addEventListener("keyup", function() {
   
-  let input = document.getElementById("pic-search");
-    userInput = input.value.toLowerCase()
+  let input = document.getElementById("pic-search");  // Receives user input from search form
+    userInput = input.value.toLowerCase() 
     
-    $('a').each(function(index, element) {
+    $('a').each(function(index, element) {            // Loops through 'a' elements
 
-      let caption = document.getElementsByTagName("a")[index].getAttribute("data-title");
+      let caption = document.getElementsByTagName("a")[index].getAttribute("data-title");    // Targets caption from 'a' element
       let match = caption.includes(userInput);
       
-
       if (match != true || match === null) {
         $('.item').eq(index).hide();
-        notFound.push('not found');
       } else {
         $('.item').eq(index).show();
-        notFound.pop();
       } 
-
-      
-      console.log(notFound.length);
-
-      // if (notFound.length > 12) {
-      //   $('#noPhotos').show(); 
-      // } else {
-      //   $('#noPhotos').hide();
-      // }
 
     });
     
-    
-  
 });
             
 
